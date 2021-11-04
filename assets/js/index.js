256f3f4add9519907e8151347a03327cda77f261
Alvin Mei <alvinkmei@gmail.com>
2021-11-04 10:13:35 -0400

    :|

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,36 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+canvas.height = 500;
+canvas.width = 1400;
+
 class Conway {
-  constructor(pixelSize, cellNum) {
-    this.pixelSize = pixelSize;
-    this.cellNum = cellNum;
-    canvas.width = pixelSize * cellNum;
+  constructor() {
+    this.pixelSize = 5;
+    this.gridHeight = Math.floor(canvas.height / this.pixelSize);
+    this.gridWidth = Math.floor(canvas.width / this.pixelSize);
+    this.deadCell = "#f7dedc";
+    this.aliveCell = "#383030";
+    this.gridA = [];
+    this.gridB = [];
   }
   createGrid() {
-    // Create grid thats cellNum*cellNum dimension
-    let gridA = [];
-    for (let i = 0; i < this.cellNum; i++) {
-      let gridInner = [];
+    // Create grid thats gridHeight*gridWidth dimension
+    for (let i = 0; i < this.gridWidth; i++) {
+      this.gridA[i] = [];
+      for (let j = 0; j < this.gridHeight; j++) {
+        this.gridA[i][j] = 0;
+      }
     }
+    this.gridB = 
+  }
+  randomizeGrid() {
+    // Randomly populates grid
   }
   populateGrid() {
     // Randomly populates grid w/ cells
+  }
+  drawGrid() {
+    // Fill canvas with grid
   }
   checkNeighbors() {
     // Returns number of living neighbors
